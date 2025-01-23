e52112a27b18ce857f3864fae66bc54c57e7ffdd
Salmoon7 <taofeekabdussalam07@gmail.com>
2025-01-23 02:50:22 -0800

    commiting

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { inter } from "@/app/ui/font";
+import { lusitana } from "@/app/ui/font";
+
+export default function Page() {
+  return (
+    <>
+      <p className={`${lusitana.className}  bg-blue-700 mb-12`}>
+        Is this really a Dashboard Page ??
+      </p>
+      <p className="bg-red-950 text-white">
+        Is this really a Dashboard Page ??
+      </p>
+    </>
+  );
+}
